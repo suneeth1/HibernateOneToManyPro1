6da8bc0987716313698bed5ce071aa577b328de0
sn301 <sn301@IN-PG02LCQL>
2021-11-24 10:06:57 +0530

    Employee.java Commit

---
+++ HibernateOneToManyPro1/src/main/java/Employee.java
@@ -0,0 +1,33 @@
+
+public class Employee {
+	private int empcode;
+	private String ename;
+	private String description;
+	private int basic;
+	public int getEmpcode() {
+		return empcode;
+	}
+	public void setEmpcode(int empcode) {
+		this.empcode = empcode;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public int getBasic() {
+		return basic;
+	}
+	public void setBasic(int basic) {
+		this.basic = basic;
+	}
+	
+
+}
